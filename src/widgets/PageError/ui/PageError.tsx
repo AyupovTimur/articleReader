68348312a237790b94aae333e55112d6737be825
AyupovTimur <timurayupov09@gmail.com>
2024-12-11 14:24:08 +0400

    Add page error

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,23 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './pageError.module.scss';
+import {useTranslation} from "react-i18next";
+
+interface PageErrorProps {
+    className?: string;
+}
+
+export const PageError = ({className}: PageErrorProps) => {
+
+  const {t} = useTranslation('pageError');
+
+  const updatePage = () => {
+      location.reload()
+  }
+
+  return (
+    <div className={classNames(cls.PageError, {}, [className])}>
+      <h1>{t("Произошла ошибка, пожалуйста обновите страницу")}</h1>
+      <button onClick={updatePage}>{t("Обновить страницу")}</button>
+    </div>
+  )
+}
