90e13f6c6b0b2b684861a4fc56cefb57e33cae1f
AyupovTimur <timurayupov09@gmail.com>
2025-01-04 09:49:29 +0400

    Update store decorator

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,11 +1,14 @@
-import {configureStore, ReducersMapObject} from '@reduxjs/toolkit'
+import {configureStore, DeepPartial, ReducersMapObject} from '@reduxjs/toolkit'
 import {StateSchema} from "app/providers/StoreProvider/config/StateSchema";
 import {counterReducer} from "entities/Counter";
 import {userReducer} from "entities/User";
 import {createReducerManager} from "app/providers/StoreProvider/config/ReducerManager";
 
-export const createReduxStore = (initialState?: StateSchema) => {
+export const createReduxStore = (
+    initialState?: StateSchema,
+    asyncReducers?: ReducersMapObject<StateSchema>) => {
     const rootReducer: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
         counter: counterReducer,
         user: userReducer,
     }
--- src/shared/config/storybook/storeDecorator/storeDecorator.tsx
+++ src/shared/config/storybook/storeDecorator/storeDecorator.tsx
@@ -1,9 +1,16 @@
 import {StoryFn } from '@storybook/react';
 import {StateSchema, StoreProvider} from "app/providers/StoreProvider";
-import {DeepPartial} from "@reduxjs/toolkit";
+import {DeepPartial, ReducersMapObject} from "@reduxjs/toolkit";
+import {loginReducer} from "features/AuthByUserName/model/slice/loginSlice";
 
-export const storeDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: StoryFn) => (
-    <StoreProvider initialState={state}>
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer,
+}
+
+export const storeDecorator = (
+    state: DeepPartial<StateSchema>,
+    asyncReducers: DeepPartial<ReducersMapObject<StateSchema>>) => (StoryComponent: StoryFn) => (
+    <StoreProvider initialState={state} asyncReducers={{...defaultAsyncReducers, ...asyncReducers}}>
         <StoryComponent/>
     </StoreProvider>
 );
